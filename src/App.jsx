bebe42a35a2a8d3b99c95c91f6d98f2edd4911cc
ajanegett <124137364+ajanegett@users.noreply.github.com>
2024-01-30 16:42:54 +0300

    new features: adding and deleting shopping elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,53 @@ import Item from "./ShopItem.jsx";
 
 function App() {
   const [text, setText] = useState("");
-  function HandleSubmit(e) { // the main code will go below...
-    e.preventDefault()
+  const [items, setItems] = useState(["example"]);
+
+  function HandleSubmit(e) {
+    // the main code will go below...
+    e.preventDefault();
+    if (text !== "") setItems(() => [...items, text]);
+    setText("");
+  }
+  function deleteItems(e) {
+    console.log(e.target.id);
+    let count = 0;
+    setItems(
+      items
+        .reverse()
+        .filter((x) => {
+          if (x !== e.target.id) return true;
+          let myBool = count === 0 ? false : true;
+          count += 1;
+          return myBool;
+        })
+        .reverse()
+    );
   }
 
   return (
-    <div className="bg-pink-300 flex flex-col items-center border border-stone-950 w-116 p-1">
+    <div
+      className="bg-pink-300 flex flex-col items-center border border-stone-950 w-116 p-1"
+      key={"div"}
+    >
       <h1 className=" block text-3xl p-5">Shopping List</h1>
       <form onSubmit={HandleSubmit}>
         <input
-          className="  p-1 w-72 block mb-3"
+          className="  p-1 w-72 block mb-6"
           type="text"
-          onChange={e => setText(e.target.value)}
+          onChange={(e) => setText(e.target.value)}
           value={text}
         />
       </form>
-      <Item textValue="aaaa" />
+      {items.map((item) => {
+        return (
+          <Item
+            textValue={item}
+            key={crypto.randomUUID()}
+            clickFunction={deleteItems}
+          />
+        );
+      })}
     </div>
   );
 }
--- src/ShopItem.jsx
+++ src/ShopItem.jsx
@@ -1,12 +1,15 @@
-import 'boxicons'
+import "boxicons";
 
-
-export default function Item({textValue}) {
-    
-    return (
-        <div className='flex w-96 justify-between text-lg font-mono items-center my-3'>
-            <p className='cursor-default'>{textValue}</p> 
-            <div className='flex justify-center items-center cursor-pointer'><box-icon name='x'></box-icon></div>
-        </div>
-    )
+export default function Item({ textValue, clickFunction }) {
+  return (
+    <div className="flex w-96 justify-between text-lg font-mono items-center my-3">
+      <p className="cursor-default">{textValue}</p>
+      <div
+        className="flex justify-center items-center cursor-pointer"
+        onClick={clickFunction}
+      >
+        <box-icon id={textValue} name="x"></box-icon>
+      </div>
+    </div>
+  );
 }
