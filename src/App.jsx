9e8b17f379e784e2e653b727205a8f2829fd550e
ajanegett <124137364+ajanegett@users.noreply.github.com>
2024-01-30 14:13:31 +0300

    finish the main UI with title and from

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [text, setText] = useState("");
+  function HandleSubmit(e) { // the main code will go below...
+    e.preventDefault()
+  }
 
   return (
-    <>
-      <div className=" text-6xl bg-yellow-700">hello mf!</div>
-    </>
-  )
+    <div className="bg-pink-300 flex flex-col items-center border border-stone-950 w-116 p-1">
+      <h1 className=" block text-3xl p-5">Shopping List</h1>
+      <form onSubmit={HandleSubmit}>
+        <input
+          className=" shadow-lg p-1 w-72 block"
+          type="text"
+          onChange={e => setText(e.target.value)}
+          value={text}
+        />
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
