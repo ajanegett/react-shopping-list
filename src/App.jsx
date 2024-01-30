3e790907d2b35285e60c9c32a2c0acf36a10ce56
ajanegett <124137364+ajanegett@users.noreply.github.com>
2024-01-30 14:58:27 +0300

    add shop item boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./index.css";
+import Item from "./ShopItem.jsx";
 
 function App() {
   const [text, setText] = useState("");
@@ -12,12 +13,13 @@ function App() {
       <h1 className=" block text-3xl p-5">Shopping List</h1>
       <form onSubmit={HandleSubmit}>
         <input
-          className=" shadow-lg p-1 w-72 block"
+          className="  p-1 w-72 block mb-3"
           type="text"
           onChange={e => setText(e.target.value)}
           value={text}
         />
       </form>
+      <Item textValue="aaaa" />
     </div>
   );
 }
--- src/ShopItem.jsx
+++ src/ShopItem.jsx
@@ -1,3 +1,12 @@
-export default function Item(textValue) {
+import 'boxicons'
+
+
+export default function Item({textValue}) {
     
+    return (
+        <div className='flex w-96 justify-between text-lg font-mono items-center my-3'>
+            <p className='cursor-default'>{textValue}</p> 
+            <div className='flex justify-center items-center cursor-pointer'><box-icon name='x'></box-icon></div>
+        </div>
+    )
 }
