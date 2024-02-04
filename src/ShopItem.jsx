3dd6edecfbb88fa8ff72cc4194c1bd93c0c6c2a1
ajanegett <124137364+ajanegett@users.noreply.github.com>
2024-02-04 11:07:27 +0300

    add supplamentary styles

--- src/ShopItem.jsx
+++ src/ShopItem.jsx
@@ -2,7 +2,7 @@ import "boxicons";
 
 export default function Item({ textValue, clickFunction, indexValue, isDone, onDDhandle}) {
   return (
-    <div id={indexValue} onDoubleClick={onDDhandle} className={`flex w-96 justify-between text-lg font-mono items-center my-3 ${isDone && " animated-strike"}`}>
+    <div id={indexValue} onDoubleClick={onDDhandle} className={`prevent-select flex w-96 justify-between text-lg font-mono items-center my-3 ${isDone && " animated-strike"}`}>
       <p id={indexValue} className="cursor-default" >{textValue}</p>
       <div
         className="flex justify-center items-center cursor-pointer"
